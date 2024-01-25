15b914a8581013e0dea2f7c0ed3b0fcc9a8e85da
binhnh0910 <107558538+binhnh0910@users.noreply.github.com>
2024-01-26 05:47:28 +0700

    phan admin va customer

---
+++ Swp391_Nhom1/ShopOnline.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Models;
+
+namespace ShopOnline.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Banh", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Keo", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Do uong", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Khac", DisplayOrder = 4 }
+                );
+        }
+    }
+}
--- Swp391_Nhom1/ShopOnline/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShopOnline.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
