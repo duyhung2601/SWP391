8e6ca24ae62817da1d2b1e785b723d3941f7d0e3
duyhung2601 <chanvoi2006@gmail.com>
2024-01-30 16:41:35 +0700

    thêm categoryName vào product ,sử dụng ajax tạo phân trang và search sắp xếp

--- Swp391_Nhom1/ShopOnline.DataAccess/obj/Debug/net7.0/ShopOnline.DataAccess.AssemblyInfo.cs
+++ Swp391_Nhom1/ShopOnline.DataAccess/obj/Debug/net7.0/ShopOnline.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopOnline.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602d08514118d14a4fcfe45f60aa0e15800d7cfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopOnline.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopOnline.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
